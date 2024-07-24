6517ca5d988fb8bdc2a10786825f594e30961982
Viet Hoang <hoangpham.04042002@gmail.com>
2024-07-25 02:18:06 +0700

    v1

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class Mycomponets extends React.Component{
+    
+    render(){
+        return (
+            <div>
+                my First Component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+
+export default Mycomponets;
