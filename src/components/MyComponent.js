96ad54886e9186fef8ab1381c74ba6859feaebc1
Viet Hoang <hoangpham.04042002@gmail.com>
2024-07-27 01:00:29 +0700

    v 10

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,11 +3,27 @@ import React from "react";
 
 class Mycomponets extends React.Component{
     
+    state = {
+        name : ' Hoang',
+        address: 'Ha Noi',
+        age: 26
+    };
+
+    handlClick(event){
+        // console.log("??? Click me")
+        console.log("my nmae is", this.state.name)
+    }
+
+    handlOnMoveOver(event) {
+        console.log(event)
+    }
+
     render(){
         return (
             <div>
-                my First Component
-                {Math.random()}
+                my name Is{this.state.name} and I'm from {this.state.address}
+                <button onMouseOver={this.handlOnMoveOver}>Click ove me</button>
+                <button onClick={this.handlClick}>Click here</button>
             </div>
         );
     }
