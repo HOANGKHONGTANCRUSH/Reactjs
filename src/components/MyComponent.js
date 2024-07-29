ff872466b71df5723d63ff1e5a1ceb29960e9714
Viet Hoang <hoangpham.04042002@gmail.com>
2024-07-29 23:35:10 +0700

    v 15

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,14 +1,20 @@
 import React from "react";
 import UserInfor from "./Userinfor";
+import DisplayInfor from "./DisplayInfor";
 
 class Mycomponets extends React.Component {
 
 
     render() {
+        const myInfor = ["ab", "ac"]
         return (
             <div>
-
                 <UserInfor></UserInfor>
+                <br></br>
+                <DisplayInfor name="Hoang" age = " 23" />
+                <DisplayInfor name="Hoang" age = " 23" />
+                <DisplayInfor name="o" age = " 3" />
+                <DisplayInfor name="Hoang" age = " 23" myInfor = {myInfor} />
             </div>
         );
     }
