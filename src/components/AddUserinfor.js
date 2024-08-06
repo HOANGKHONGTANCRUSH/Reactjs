132f67ff59ef9b03e9c5e7824637d88a5d75ca30
Viet Hoang <hoangpham.04042002@gmail.com>
2024-08-07 02:40:13 +0700

    v 24

--- src/components/AddUserinfor.js
+++ src/components/AddUserinfor.js
@@ -1,58 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 
-class AddUserinfor extends React.Component {
+// class AddUserinfor extends React.Component {
 
-    state = {
-        name: " ",
-        address: " ",
-        age: "",
-    };
-    handlOnMoveOver(event) {
-        console.log(event);
+//     state = {
+//         name: " ",
+//         address: " ",
+//         age: "",
+//     };
+//     handlOnMoveOver(event) {
+//         console.log(event);
+//     }
+
+//     handlOnChangeInput = (event) => {
+//         this.setState({
+//             name: event.target.value
+//         })
+//     }
+
+//     handlOnChangeage = (event) => {
+//         this.setState({
+//             age: event.target.value
+//         })
+//     }
+
+//     handleOnsubmit = (event) => {
+//         event.preventDefault();
+//         this.props.handleAddNewUser({
+//             id: Math.floor((Math.random() * 100) + 1) + "-Random",
+//             name: this.state.name,
+//             age: this.state.age
+//         })
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 my name Is {this.state.name} and I'm from {this.state.age}
+//                 <form onSubmit={(event) => this.handleOnsubmit(event)} >
+//                     <label>Your Name :</label>
+//                     <input type="text"
+//                         value={this.state.name}
+//                         onChange={(event) => this.handlOnChangeInput(event)}
+//                     />
+//                     <button>Sumit</button>
+
+//                     <label>Your Age :</label>
+//                     <input type="text"
+//                         value={this.state.age}
+//                         onChange={(event) => this.handlOnChangeage(event)}
+//                     />
+//                     <button>Sumit</button>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+const AddUserinfor = (props) => {
+
+    // state = {
+    //     name: " ",
+    //     address: " ",
+    //     age: "",
+    // };
+    const [name, setName] = useState("");
+    const [address, setAddress] = useState("");
+    const [age, setAge] = useState("");
+
+    const handlOnChangeInput = (event) => {
+        // this.setState({
+        //     name: event.target.value
+        // })
+        setName(event.target.value)
     }
 
-    handlOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
+    const handlOnChangeage = (event) => {
+        // this.setState({
+        //     age: event.target.value
+        // })
+        setAge(event.target.value)
     }
 
-    handlOnChangeage = (event) => {
-        this.setState({
-            age: event.target.value
-        })
-    }
-
-    handleOnsubmit = (event) => {
+    const handleOnsubmit = (event) => {
         event.preventDefault();
-        this.props.handleAddNewUser({
+        props.handleAddNewUser({
             id: Math.floor((Math.random() * 100) + 1) + "-Random",
-            name: this.state.name,
-            age: this.state.age
+            name: name,
+            age: age
         })
     }
-    render() {
-        return (
-            <div>
-                my name Is {this.state.name} and I'm from {this.state.age}
-                <form onSubmit={(event) => this.handleOnsubmit(event)} >
-                    <label>Your Name :</label>
-                    <input type="text"
-                        value={this.state.name}
-                        onChange={(event) => this.handlOnChangeInput(event)}
-                    />
-                    <button>Sumit</button>
+    return (
+        <div>
+            my name Is {name} and I'm from {age}
+            <form onSubmit={(event) => handleOnsubmit(event)} >
+                <label>Your Name :</label>
+                <input type="text"
+                    value={name}
+                    onChange={(event) => handlOnChangeInput(event)}
+                />
+                <button>Sumit</button>
 
-                    <label>Your Age :</label>
-                    <input type="text"
-                        value={this.state.age}
-                        onChange={(event) => this.handlOnChangeage(event)}
-                    />
-                    <button>Sumit</button>
-                </form>
-            </div>
-        )
-    }
+                <label>Your Age :</label>
+                <input type="text"
+                    value={age}
+                    onChange={(event) => handlOnChangeage(event)}
+                />
+                <button>Sumit</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddUserinfor;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,48 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUserinfor from "./AddUserinfor";
 import DisplayInfor from "./DisplayInfor";
 
-class Mycomponets extends React.Component {
+// class Mycomponets extends React.Component {
 
-    state = {
-        listUser: [
+//     state = {
+//         listUser: [
+//             { id: 1, name: "hoang", age: "39" },
+//             { id: 2, name: "niec", age: "98" },
+//             { id: 3, name: "wow", age: "3" },
+//         ]
+//     }
+
+//     handleAddNewUser = (userObj) => {
+
+//         this.setState({
+//             listUser: [userObj, ...this.state.listUser]
+//         })
+//     }
+//     handleDeleteUser = (userId) => {
+//         let listUserClone = this.state.listUser
+//         listUserClone = listUserClone.filter(item => item.id !== userId)
+//         this.setState({
+//             listUser: listUserClone
+//         })
+
+//     }
+
+//     render() {
+
+//         return (
+//             <>
+//                 <div>
+//                     <AddUserinfor
+//                         handleAddNewUser={this.handleAddNewUser}
+//                     />
+//                     <br></br>
+//                     <DisplayInfor listUser={this.state.listUser}
+//                         handleDeleteUser={this.handleDeleteUser}
+//                     />
+//                 </div>
+//             </>
+//         );
+//     }
+// }
+
+const Mycomponets = (props) => {
+
+    const [listUser, setlistUser] = useState(
+        [
             { id: 1, name: "hoang", age: "39" },
             { id: 2, name: "niec", age: "98" },
             { id: 3, name: "wow", age: "3" },
         ]
-    }
+    )
 
-    handleAddNewUser = (userObj) => {
-
-        this.setState({
-            listUser: [userObj, ...this.state.listUser]
-        })
+    const handleAddNewUser = (userObj) => {
+        setlistUser([userObj, ...listUser])
+        // this.setState({
+        //     listUser: [userObj, ...this.state.listUser]
+        // })
     }
-    handleDeleteUser = (userId) => {
-        let listUserClone = this.state.listUser
+    let handleDeleteUser = (userId) => {
+        let listUserClone = listUser
         listUserClone = listUserClone.filter(item => item.id !== userId)
-        this.setState({
-            listUser: listUserClone
-        })
-
+        setlistUser(listUserClone)
     }
-
-    render() {
-
-        return (
-            <>
-                <div>
-                    <AddUserinfor
-                        handleAddNewUser={this.handleAddNewUser}
-                    />
-                    <br></br>
-                    <DisplayInfor listUser={this.state.listUser}
-                        handleDeleteUser={this.handleDeleteUser}
-                    />
-                </div>
-            </>
-        );
-    }
+    return (
+        <>
+            <div>
+                <AddUserinfor
+                    handleAddNewUser={handleAddNewUser}
+                />
+                <br></br>
+                <DisplayInfor listUser={listUser}
+                    handleDeleteUser={handleDeleteUser}
+                />
+            </div>
+        </>
+    );
 }
 
 export default Mycomponets;
