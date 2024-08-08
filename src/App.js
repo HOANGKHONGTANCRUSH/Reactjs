86dfbe786299290611cc88a6e7692bae9e56292a
Viet Hoang <hoangpham.04042002@gmail.com>
2024-08-09 03:03:54 +0700

    v 26

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import logo from "./logo.svg";
 import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import Mycomponets from "./components/MyComponent";
 import React from "react";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <Mycomponets></Mycomponets>
-        </div>
-      </div>
-    );
-  }
+
+const App = () => {
+  return (
+    <div className="app-container">
+      hello word
+    </div>
+  )
 }
 
 export default App;
