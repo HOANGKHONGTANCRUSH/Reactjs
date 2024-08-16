6d11fbf5664db1d0075a7e3f7b890c9de4b2eb8d
Viet Hoang <hoangpham.04042002@gmail.com>
2024-08-17 00:24:52 +0700

    v 33

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -3,14 +3,30 @@ import "./Admin.scss";
 import { FaBars } from "react-icons/fa";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false);
+
+    const notify = () => {
+        toast('🦄 Wow so easy!', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+    };
+
     return (
         <div className="admin-container">
             <div className="admin-sidebar">
-                <SideBar
-                    collapsed={collapsed} />
+                <SideBar collapsed={collapsed} />
             </div>
             <div className="admin-content">
                 <div className="admin-header">
@@ -20,7 +36,10 @@ const Admin = (props) => {
                     <Outlet />
                 </div>
             </div>
+            <button onClick={notify}>Show Toast</button>
+            <ToastContainer />
         </div>
-    )
-}
-export default Admin
+    );
+};
+
+export default Admin;
--- src/components/Admin/content/ModalCreateUser.js
+++ src/components/Admin/content/ModalCreateUser.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from "react-icons/fc";
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props
@@ -34,16 +35,30 @@ const ModalCreateUser = (props) => {
 
         console.log("hallo", event.target.files[0])
     }
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
 
     const handlSubmitCreateUser = async () => {
-        // let data = {
-        //     email: email,
-        //     password: password,
-        //     username: username,
-        //     role: role,
-        //     userImage: image
-        // }
-        // console.log(">>>>", data)
+        // validate
+        const isValiEmail = validateEmail(email);
+
+        if (!isValiEmail) {
+            toast.error("Invalid email 🦄")
+            // toast.success()
+            // toast.info()
+            return;
+        }
+
+        if (!password) {
+            toast.error("invalid password")
+            return;
+        }
+
 
         const data = new FormData();
         data.append('email', email);
@@ -53,7 +68,15 @@ const ModalCreateUser = (props) => {
         data.append('userImage', image);
 
         let res = await axios.post('http://localhost:8081/api/v1/participant', data)
-        console.log(">>>> res", res)
+        console.log(">>>> res", res.data)
+        if (res.data && res.data.EC === 0) {
+            toast.success(res.data.EM)
+            handleClose();
+        }
+
+        if (res.data && res.data.EC !== 0) {
+            toast.error(res.data.EM)
+        }
     }
 
     return (
