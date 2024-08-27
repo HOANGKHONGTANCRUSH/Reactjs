924e647ac18cfdad31cb32ae4e53a4d80213caa3
Viet Hoang <hoangpham.04042002@gmail.com>
2024-08-27 16:13:59 +0700

    v 34

--- src/components/Admin/content/ModalCreateUser.js
+++ src/components/Admin/content/ModalCreateUser.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from "react-icons/fc";
-import axios from 'axios';
 import { toast } from 'react-toastify';
+import { postCreateNewUser } from '../../../services/apiSevice';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props
@@ -45,37 +45,30 @@ const ModalCreateUser = (props) => {
 
     const handlSubmitCreateUser = async () => {
         // validate
-        const isValiEmail = validateEmail(email);
-
-        if (!isValiEmail) {
-            toast.error("Invalid email 🦄")
-            // toast.success()
-            // toast.info()
-            return;
-        }
-
+        // const isValiEmail = validateEmail(email);
+        // if (!isValiEmail) {
+        //     toast.error("Invalid email 🦄")
+        //     // toast.success()
+        //     // toast.info()
+        //     return;
+        // }
         if (!password) {
             toast.error("invalid password")
             return;
         }
 
 
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
 
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data)
-        console.log(">>>> res", res.data)
-        if (res.data && res.data.EC === 0) {
-            toast.success(res.data.EM)
+
+        let data = await postCreateNewUser(email, password, username, role, image)
+        console.log(">>>> res", data)
+        if (data && data.EC === 0) {
+            toast.success(data.EM)
             handleClose();
         }
 
-        if (res.data && res.data.EC !== 0) {
-            toast.error(res.data.EM)
+        if (data && data.EC !== 0) {
+            toast.error(data.EM)
         }
     }
 
